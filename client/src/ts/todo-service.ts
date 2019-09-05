8c577f334f0cac5dcd6c779189919962cca81d8b
Romain Freydiger <freydiger.r@sfeir.com>
2019-09-05 14:35:50 +0200

    generic correction

--- client/src/ts/todo-service.ts
+++ client/src/ts/todo-service.ts
@@ -1,41 +1,45 @@
+import { BackendResponse } from './interfaces/backend-response';
+import { Todo } from './interfaces/todo';
+import { Task } from './interfaces/task';
+
 export class TodoService {
 
   private apiUrl = 'http://localhost:9000/api';
 
-  findAll() {
+  findAll(): JQuery.jqXHR<BackendResponse<Todo[]>> {
     return $.ajax({
       url: `${this.apiUrl}/all`
     });
   }
 
-  findMine() {
+  findMine(): JQuery.jqXHR<BackendResponse<Task[]>> {
     return $.ajax({
       url: `${this.apiUrl}/my`
     });
   }
 
-  delete(id) {
+  delete(id: string): JQuery.jqXHR<void> {
     return $.ajax({
       method: 'DELETE',
       url: `${this.apiUrl}/my/${id}`
     });
   }
 
-  toInProgress(id) {
+  toInProgress(id: string): JQuery.jqXHR<void> {
     return $.ajax({
       method: 'PUT',
       url: `${this.apiUrl}/my/${id}/in-progress`
     });
   }
 
-  toDone(id){
+  toDone(id: string): JQuery.jqXHR<void> {
     return $.ajax({
       method: 'PUT',
       url: `${this.apiUrl}/my/${id}/done`
     });
   }
 
-  create(task) {
+  create(task: Task): JQuery.jqXHR<BackendResponse<Todo>> {
     return $.ajax({
       method: 'PUT',
       data: task,
